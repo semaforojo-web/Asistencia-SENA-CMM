e61fdf1c7356159a4a9b50d21e49c081ad8efe8a
semaforojo-web <semaforojo@gmail.com>
2026-07-09 17:36:33 -0500

    Update app.py

--- app.py
+++ app.py
@@ -388,7 +388,7 @@ with tab4:
             if df_preview.shape[1] > 47:
                 df_resumen = pd.DataFrame({
                     "Grupo": df_preview[6], "Instructor": df_preview[5],
-                    "Número Asignación": df_preview[3], "Asignatura": df_preview[10], "Trimestre": df_preview[47]
+                    "Número Asignación": df_preview[3], "Resultados": df_preview[10], "Trimestre": df_preview[47]
                 }).dropna(subset=["Grupo", "Instructor"], how="all")
                 st.dataframe(df_resumen, use_container_width=True)
                 
