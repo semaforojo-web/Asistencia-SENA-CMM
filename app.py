1915d099f9d53ddfc4082f871f533062dd809041
semaforojo-web <semaforojo@gmail.com>
2026-07-11 00:25:28 -0500

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ st.set_page_config(page_title="Control de Asistencia y Evaluación - SENA", layo
 
 # Nombre del archivo original de Excel en tu repositorio de GitHub
 DB_FILE = "Reporte de Asistencia.xlsx"
-REPO_NAME = "semaforojo-web/asistencia-sena-cmm"
+REPO_NAME = "semaforojo-web/Asistencia-SENA-CMM"
 
 # ==========================================
 # FUNCIÓN AUXILIAR: DESCARGAR EXCEL DE GITHUB A MEMORIA
